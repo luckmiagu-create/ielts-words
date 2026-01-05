b02a90fbaa4033c49bde2192697259c0626ab592
luckmiagu-create <luckmiagu@gmail.com>
2026-01-05 11:14:57 +0800

    Update icon filenames in code

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,25 @@
-const CACHE_NAME = "ielts-words-v1";
+const CACHE_NAME = 'ielts-words-v4';
 const urlsToCache = [
-  "./",
-  "./ielts_words.html",
+  "./index.html",
   "./manifest.json",
-  "./icon.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache initialized with correct files');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
-
 
